2f5ebcd07a247f312396ce746cd4e71dd627fd95
adin3101 <105016669+adin3101@users.noreply.github.com>
2024-08-10 13:44:12 +0530

    Update friendRequestsController.js

--- controllers/friendRequestsController.js
+++ controllers/friendRequestsController.js
@@ -1,5 +1,5 @@
-import friendRequests from "../models/friendRequests";
-import User from "../models/User";
+import friendRequests from "../models/friendRequests.js";
+import User from "../models/User.js";
 
 export const sendFriendRequest = async(req,res)=>{
     try {
